c63f9da5e1ff8f5be0861fd90a7dba5a62fa5fa7
dbc-apprentice <apprentice@devbootcamp.com>
2013-07-15 16:56:42 -0500

    setup new user forms

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,17 @@
 class UsersController < ApplicationController
   def index
-    
+    @users = User.all
   end
+
+  def new
+  end
+
+  def create
+    p "======== HERE'S THE PARAMS! ========="
+    p params
+    @user = User.create(params[:user])
+    redirect_to user_path(@user)
+  end
+
+
 end
--- config/environment.rb
+++ config/environment.rb
@@ -1,7 +1,7 @@
+require 'bcrypt'
 # Load the rails application
 require File.expand_path('../application', __FILE__)
 
 # Initialize the rails application
 Craigslist::Application.initialize!
 
-require 'bcrypt'

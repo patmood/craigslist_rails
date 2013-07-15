a74fb9138350daa78b6aa4c959f19733c61821c0
TH <tylerhartland7@gmail.com>
2013-07-15 18:16:01 -0500

    RESTful posts

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,4 +1,33 @@
 class PostsController < ApplicationController
-  def index
+    def index
+    @posts = Post.all
+  end
+
+  def new
+    @post = Post.new
+  end
+
+  def create
+    @post = Post.create(params[:post])
+    redirect_to post_path(@post)
+  end
+
+  def show
+    @post = Post.find(params[:id])
+  end
+
+  def edit
+    @post = Post.find(params[:id])
+  end
+
+  def update
+    @post = Post.find(params[:id])
+    @post.update_attributes(params[:post])
+    redirect_to post_path(@post)
+  end
+
+  def destroy
+    Post.destroy(params[:id])
+    redirect_to posts_path
   end
 end

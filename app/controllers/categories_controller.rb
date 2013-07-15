945194b22c42ef8813c5b0d03af6759f2bbb182e
TH <tylerhartland7@gmail.com>
2013-07-15 17:56:41 -0500

    RESTful categories

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -2,4 +2,33 @@ class CategoriesController < ApplicationController
   def index
     @categories = Category.all
   end
+
+  def new
+    @category = Category.new
+  end
+
+  def create
+    @category = Category.create(params[:category])
+    redirect_to category_path(@category)
+  end
+
+  def show
+    @category = Category.find(params[:id])
+  end
+
+  def edit
+    @category = Category.find(params[:id])
+  end
+
+  def update
+    @category = Category.find(params[:id])
+    @category.update_attributes(params[:category])
+    redirect_to category_path(@category)
+  end
+
+  def destroy
+    Category.destroy(params[:id])
+    redirect_to categories_path
+  end
+
 end
